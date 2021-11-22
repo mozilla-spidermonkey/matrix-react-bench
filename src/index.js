8ae2dc0e3a7b427d8938aeba9cde73d257cdcc3e
Ted Campbell <tcampbell@mozilla.com>
2021-11-22 17:46:08 -0500

    Refactor synthesis of room data

--- src/index.js
+++ src/index.js
@@ -31,75 +31,88 @@ MatrixClientPeg.matrixClient = client;
 DMRoomMap.makeShared().start();
 
 let event_ts = Date.now();
+let event_idx = 1000;
 
-let user_id = "@user:example.org";
+let next_event_id = () => `\$${event_idx++}:localhost`;
+
+let user1_id = "@user:example.org";
 let user2_id = "@other:example.org";
+
+function synthesize_room(room_id) {
+    let room_opts = {
+        pendingEventOrdering: PendingEventOrdering.Detached,
+        unstableClientRelationAggregation: true,
+    };
+
+    let room = new Room(room_id, client, user1_id, room_opts);
+
+    room.addLiveEvents([
+        new MatrixEvent({
+            event_id: next_event_id(),
+            type: EventType.RoomCreate,
+            content: {
+                creator: user1_id,
+                room_version: ROOM_VERSION,
+            },
+            sender: user1_id,
+            room_id,
+            origin_server_ts: event_ts,
+            unsigned: {},
+            state_key: "",
+        }),
+        new MatrixEvent({
+            event_id: next_event_id(),
+            type: EventType.RoomMember,
+            content: {
+                displayname: "username",
+                membership: "join",
+            },
+            sender: user1_id,
+            room_id,
+            origin_server_ts: event_ts,
+            unsigned: {},
+            state_key: user1_id,
+        }),
+        new MatrixEvent({
+            event_id: next_event_id(),
+            type: EventType.RoomMember,
+            content: {
+                displayname: "other",
+                membership: "join",
+            },
+            sender: user2_id,
+            room_id,
+            origin_server_ts: event_ts,
+            unsigned: {},
+            state_key: user2_id,
+        }),
+        new MatrixEvent({
+            event_id: next_event_id(),
+            type: EventType.RoomMessage,
+            content: ContentHelpers.makeTextMessage("Room test message"),
+            sender: user1_id,
+            room_id,
+            origin_server_ts: event_ts,
+            unsigned: {},
+        }),
+        new MatrixEvent({
+            event_id: next_event_id(),
+            type: EventType.RoomMessage,
+            content: ContentHelpers.makeTextMessage("Reply test message"),
+            sender: user2_id,
+            room_id,
+            origin_server_ts: event_ts,
+            unsigned: {},
+        }),
+    ]);
+
+    room.updateMyMembership("join");
+
+    return room;
+}
+
 let room_id = "!AAAAAAAAAAAA:example.org";
-let room_opts = {
-    pendingEventOrdering: PendingEventOrdering.Detached,
-    unstableClientRelationAggregation: true,
-};
-client.room = new Room(room_id, client, user_id, room_opts);
-client.room.addLiveEvents([
-    new MatrixEvent({
-        event_id: "$100:localhost",
-        type: EventType.RoomCreate,
-        content: {
-            creator: user_id,
-            room_version: ROOM_VERSION,
-        },
-        sender: user_id,
-        room_id,
-        origin_server_ts: event_ts,
-        unsigned: {},
-        state_key: "",
-    }),
-    new MatrixEvent({
-        event_id: "$101:localhost",
-        type: EventType.RoomMember,
-        content: {
-            displayname: "username",
-            membership: "join",
-        },
-        sender: user_id,
-        room_id,
-        origin_server_ts: event_ts,
-        unsigned: {},
-        state_key: user_id,
-    }),
-    new MatrixEvent({
-        event_id: "$102:localhost",
-        type: EventType.RoomMember,
-        content: {
-            displayname: "other",
-            membership: "join",
-        },
-        sender: user2_id,
-        room_id,
-        origin_server_ts: event_ts,
-        unsigned: {},
-        state_key: user2_id,
-    }),
-    new MatrixEvent({
-        event_id: "$200:localhost",
-        type: EventType.RoomMessage,
-        content: ContentHelpers.makeTextMessage("Room test message"),
-        sender: user_id,
-        room_id,
-        origin_server_ts: event_ts,
-        unsigned: {},
-    }),
-    new MatrixEvent({
-        event_id: "$201:localhost",
-        type: EventType.RoomMessage,
-        content: ContentHelpers.makeTextMessage("Reply test message"),
-        sender: user2_id,
-        room_id,
-        origin_server_ts: event_ts,
-        unsigned: {},
-    }),
-]);
-client.room.updateMyMembership("join");
+client.room = synthesize_room(room_id);
 
 defaultDispatcher.dispatch({
     action: "view_room",
